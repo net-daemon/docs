f3c4579c4b5248a90503fe14239fbac090e15c23
Tomas Hellström <tomas.hellstrom@yahoo.se>
2021-11-28 17:41:20 +0100

    add app model and extensions docs

--- sidebars.js
+++ sidebars.js
@@ -15,7 +15,11 @@ module.exports = {
           'started/hacs']
       },
     ],
-
+      'AppModel API': [
+        'app_model/app_model',
+        'app_model/app_model_instancing',
+        'app_model/app_model_advanced_config',
+      ],
       'HassModel API': [
         'hass_model/hass_model',
         'hass_model/hass_model_codegen',
@@ -24,9 +28,14 @@ module.exports = {
         'hass_model/hass_model_working_with_entities',
         'hass_model/hass_model_migration'
       ],
-
-
-    'V2 API': [
+      'Extensions API': [
+        {
+          'Scheduling': [
+            'extensions/scheduling/extensions_scheduling',
+          ]
+        },
+      ],
+    'V2 API (deprecated)': [
       'api/api',
       'api/api_entities',
       'api/api_state',
