13ebcde002d1f741bd9f7f752503d223ac799ac7
Tomas <tomas.hellstrom@yahoo.se>
2026-05-07 08:35:37 +0200

    Make docs breadcrumbs clickable

--- sidebars.js
+++ sidebars.js
@@ -1,86 +1,121 @@
 module.exports = {
-  someSidebar: {
-    Documentation: [
-      'user/index',
-      'user/user_support_policy',
-      'user/whats_new_v5', 
-      {
-        type: 'category',
-        label: 'Getting started',
-        items: [
-          'user/started/get_started',
-          'user/started/installation',
-          'user/started/integration',
-          'user/started/example',
-        ]
+  someSidebar: [
+    {
+      type: 'category',
+      label: 'Documentation',
+      link: {
+        type: 'doc',
+        id: 'user/index',
       },
-      {
-        type: 'category',
-        label: 'AppModel',
-        items: [
-          'user/app_model/app_model',
-          'user/app_model/app_model_instancing',
-          'user/app_model/app_model_advanced_config',
-          'user/app_model/app_model_custom_logging',
-          'user/app_model/app_model_custom_config',
-          'user/app_model/app_model_moving_from_v3',
-        ],
-      },
-      {
-        type: 'category',
-        label: 'HassModelApi',
-        items: [
-          'user/hass_model/hass_model',
-          'user/hass_model/hass_model_codegen',
-          'user/hass_model/hass_model_generated_entities',
-          'user/hass_model/hass_model_generated_service',
-          'user/hass_model/hass_model_working_with_entities',
-          'user/hass_model/hass_model_subscribe_to_triggers',
-          'user/hass_model/hass_model_events',
-          'user/hass_model/hass_model_notifications',
-          'user/hass_model/hass_model_integration_servicecallback',
-          'user/hass_model/hass_model_registry',          
-          'user/hass_model/hass_model_service_with_returnvalue',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Advanced',
-        items: [
-          'user/advanced/call_ha_api',
-          'user/advanced/async_features',
-          'user/advanced/dev_workflow3',
-          'user/advanced/advanced_client',
-          'user/advanced/application_lifecycle',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Extensions API',
-        items: [
-          'user/extensions/extensions_scheduling',
-          'user/extensions/extensions_tts',
-          'user/extensions/extensions_mqttentitymanager',
-        ]
-      },
-      {
-        type:'category',
-        label: 'Tutorials',
-        items: [
-          'user/tutorials/publish_script',
-          'user/tutorials/webhost',
-        ]
-      },
-      {
-        type: 'category',
-        label: 'Troubleshooting',
-        items: [
-          'user/troubleshooting/trouble_call_service',
-          'user/troubleshooting/unauthorized'
-        ]
-      },
-    ],
-  },
+      items: [
+        'user/user_support_policy',
+        'user/whats_new_v5',
+        {
+          type: 'category',
+          label: 'Getting started',
+          link: {
+            type: 'doc',
+            id: 'user/started/get_started',
+          },
+          items: [
+            'user/started/get_started',
+            'user/started/installation',
+            'user/started/integration',
+            'user/started/example',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'AppModel',
+          link: {
+            type: 'doc',
+            id: 'user/app_model/app_model',
+          },
+          items: [
+            'user/app_model/app_model',
+            'user/app_model/app_model_instancing',
+            'user/app_model/app_model_advanced_config',
+            'user/app_model/app_model_custom_logging',
+            'user/app_model/app_model_custom_config',
+            'user/app_model/app_model_moving_from_v3',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'HassModelApi',
+          link: {
+            type: 'doc',
+            id: 'user/hass_model/hass_model',
+          },
+          items: [
+            'user/hass_model/hass_model',
+            'user/hass_model/hass_model_codegen',
+            'user/hass_model/hass_model_generated_entities',
+            'user/hass_model/hass_model_generated_service',
+            'user/hass_model/hass_model_working_with_entities',
+            'user/hass_model/hass_model_subscribe_to_triggers',
+            'user/hass_model/hass_model_events',
+            'user/hass_model/hass_model_notifications',
+            'user/hass_model/hass_model_integration_servicecallback',
+            'user/hass_model/hass_model_registry',
+            'user/hass_model/hass_model_service_with_returnvalue',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Advanced',
+          link: {
+            type: 'doc',
+            id: 'user/advanced/call_ha_api',
+          },
+          items: [
+            'user/advanced/call_ha_api',
+            'user/advanced/async_features',
+            'user/advanced/dev_workflow3',
+            'user/advanced/advanced_client',
+            'user/advanced/application_lifecycle',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Extensions API',
+          link: {
+            type: 'doc',
+            id: 'user/extensions/extensions_scheduling',
+          },
+          items: [
+            'user/extensions/extensions_scheduling',
+            'user/extensions/extensions_tts',
+            'user/extensions/extensions_mqttentitymanager',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Tutorials',
+          link: {
+            type: 'doc',
+            id: 'user/tutorials/publish_script',
+          },
+          items: [
+            'user/tutorials/publish_script',
+            'user/tutorials/webhost',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Troubleshooting',
+          link: {
+            type: 'doc',
+            id: 'user/troubleshooting/trouble_call_service',
+          },
+          items: [
+            'user/troubleshooting/trouble_call_service',
+            'user/troubleshooting/unauthorized',
+          ],
+        },
+      ],
+    },
+  ],
   "developer documentation": {
     "NetDaemon development": [
       "developer/developer",
